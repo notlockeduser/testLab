f53aaa2476d5e0a25c9bc861a1f724156e41f8b1
Holovin <holovin.b@ajax.systems>
2022-06-10 15:52:08 +0300

    Initial commit

---
+++ src/main/java/corp/HelloWorld.java
@@ -0,0 +1,10 @@
+package corp;
+
+public class HelloWorld {
+
+    public String helloWorld() {
+
+        System.out.println("Hello world");
+        return "Hello world";
+    }
+}
---
+++ src/test/java/corp/HelloWorldTest.java
@@ -0,0 +1,26 @@
+package corp;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    void test() {
+
+        var helloWorld = new HelloWorld();
+        assertThat("Hello world").isEqualTo(helloWorld.helloWorld());
+    }
+
+    @Test
+    void test2() {
+        var helloWorld = new HelloWorld();
+        assertThat("Hello w0rld").isEqualTo(helloWorld.helloWorld());
+    }
+
+    @Test
+    void test3() {
+        var helloWorld = new HelloWorld();
+        assertThat("Hello world").isEqualTo(helloWorld.helloWorld());
+    }
+}
